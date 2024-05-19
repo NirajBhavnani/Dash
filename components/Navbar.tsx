19a9652ff522deeea77c16dba82188405a04896a
nbhavna <niraj.bhavnani@morningstar.com>
2024-05-19 23:31:17 +0530

    Navbar feature added, shadcn sheet component usage and custom tailwind classes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileNavbar from "./MobileNavbar";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    // flex-between is a custom class written in globals.css (No mystery without using flex)
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          height={32}
+          width={32}
+          alt="Dash logo"
+          className="max-sm:size-10"
+        />
 
-export default Navbar
+        {/* In tailwind css, we use [] whenever we want to apply specific values */}
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Dash
+        </p>
+      </Link>
+
+      {/* For mobile devices */}
+      <div className="flex-between gap-5">
+        {/* TODO: Clerk- User Management */}
+
+        <MobileNavbar />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
